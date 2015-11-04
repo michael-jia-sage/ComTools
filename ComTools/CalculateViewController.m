37cc2eb06c611f142751eb6cade692bf482a3383
Zonggao Jia <jiazonggao@zonggao-jias-computer.local>
2015-11-03 22:04:59 -0800

    Tip Calcuator

--- ComTools/CalculateViewController.m
+++ ComTools/CalculateViewController.m
@@ -9,19 +9,44 @@
 #import "CalculateViewController.h"
 
 @interface CalculateViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *txtTotal;
+@property (weak, nonatomic) IBOutlet UISlider *sldTipRate;
+@property (weak, nonatomic) IBOutlet UILabel *lblTipAmount;
+@property (weak, nonatomic) IBOutlet UILabel *lblTip1;
+@property (weak, nonatomic) IBOutlet UILabel *lblTip2;
+@property (weak, nonatomic) IBOutlet UILabel *lblTip3;
+@property (weak, nonatomic) IBOutlet UILabel *lblTip4;
+@property (weak, nonatomic) IBOutlet UILabel *lblTip5;
+@property (weak, nonatomic) IBOutlet UILabel *lblTipRate;
 
 @end
 
 @implementation CalculateViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)TotalChanged:(id)sender {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    float total = [self.txtTotal.text floatValue];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    self.lblTipAmount.text = [NSString stringWithFormat:@"Tip is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: (total * self.sldTipRate.value)]]];
+    self.lblTip1.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.08f)]];
+    self.lblTip2.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.1f)]];
+    self.lblTip3.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.12f)]];
+    self.lblTip4.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.15f)]];
+    self.lblTip5.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.18f)]];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)RateChanged:(id)sender {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    float total = [self.txtTotal.text floatValue];
+    self.lblTipRate.text = [NSString stringWithFormat:@"%.2f%%", self.sldTipRate.value * 100];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    self.lblTipAmount.text = [NSString stringWithFormat:@"Tip is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: (total * self.sldTipRate.value)]]];
+    self.lblTip1.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.08f)]];
+    self.lblTip2.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.1f)]];
+    self.lblTip3.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.12f)]];
+    self.lblTip4.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.15f)]];
+    self.lblTip5.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.18f)]];
 }
+
+//- CalculateTip
 
 @end
