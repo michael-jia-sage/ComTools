f598bb30dd83a0ec1a4a9cbcf3292602785cccfe
Zonggao Jia <jiazonggao@zonggao-jias-computer.local>
2015-11-04 22:03:20 -0800

    Add group split feature

--- ComTools/CalculateViewController.m
+++ ComTools/CalculateViewController.m
@@ -18,35 +18,45 @@
 @property (weak, nonatomic) IBOutlet UILabel *lblTip4;
 @property (weak, nonatomic) IBOutlet UILabel *lblTip5;
 @property (weak, nonatomic) IBOutlet UILabel *lblTipRate;
+@property (weak, nonatomic) IBOutlet UILabel *lblTotalAmount;
+@property (weak, nonatomic) IBOutlet UIStepper *stepGroupSize;
+@property (weak, nonatomic) IBOutlet UILabel *lblGroupSize;
+@property (weak, nonatomic) IBOutlet UILabel *lblSplitAmount;
 
 @end
 
 @implementation CalculateViewController
-- (IBAction)TotalChanged:(id)sender {
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    if (![[touch view] isKindOfClass:[UITextField class]]) {
+        [self.view endEditing:YES];
+    }
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (IBAction)DoCalculate:(id)sender {
     NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
     float total = [self.txtTotal.text floatValue];
+    float tipRate = self.sldTipRate.value;
+    float tip = total * tipRate;
+    self.lblTipRate.text = [NSString stringWithFormat:@"%.2f%%", tipRate * 100];
     [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-    self.lblTipAmount.text = [NSString stringWithFormat:@"Tip is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: (total * self.sldTipRate.value)]]];
+    self.lblTipAmount.text = [NSString stringWithFormat:@"Tip is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: tip]]];
     self.lblTip1.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.08f)]];
     self.lblTip2.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.1f)]];
     self.lblTip3.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.12f)]];
     self.lblTip4.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.15f)]];
     self.lblTip5.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.18f)]];
+    
+    float totalAmount = total + tip;
+    self.lblTotalAmount.text = [NSString stringWithFormat:@"Total is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: totalAmount]]];
+    
+    double groupSize = self.stepGroupSize.value;
+    self.lblSplitAmount.text = [formatter stringFromNumber: [NSNumber numberWithDouble: totalAmount / groupSize]];
+    
+    [formatter setNumberStyle:NSNumberFormatterNoStyle];
+    self.lblGroupSize.text = [formatter stringFromNumber: [NSNumber numberWithDouble: groupSize]];
 }
-
-- (IBAction)RateChanged:(id)sender {
-    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-    float total = [self.txtTotal.text floatValue];
-    self.lblTipRate.text = [NSString stringWithFormat:@"%.2f%%", self.sldTipRate.value * 100];
-    [formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-    self.lblTipAmount.text = [NSString stringWithFormat:@"Tip is %@", [formatter stringFromNumber: [NSNumber numberWithFloat: (total * self.sldTipRate.value)]]];
-    self.lblTip1.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.08f)]];
-    self.lblTip2.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.1f)]];
-    self.lblTip3.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.12f)]];
-    self.lblTip4.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.15f)]];
-    self.lblTip5.text = [formatter stringFromNumber: [NSNumber numberWithFloat: (total * 0.18f)]];
-}
-
-//- CalculateTip
 
 @end
