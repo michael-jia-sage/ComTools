cab93d3ad160139814216a572f793da629aa5f67
Michael Jia <Michael.Jia@sage.com>
2015-11-06 17:47:49 -0800

    Table views

--- ComTools/CurrencyViewController.h
+++ ComTools/CurrencyViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CurrencyViewController : UIViewController
+@interface CurrencyViewController : UIViewController<UITableViewDataSource>
 
 @end
--- ComTools/CurrencyViewController.m
+++ ComTools/CurrencyViewController.m
@@ -12,16 +12,45 @@
 
 @interface CurrencyViewController () <CRCurrencyRequestDelegate>
 @property (nonatomic) CRCurrencyRequest *req;
+@property (nonatomic) CRCurrencyResults *res;
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UIButton *convertButton;
 @property (weak, nonatomic) IBOutlet UILabel *currencyA;
 @property (weak, nonatomic) IBOutlet UILabel *currencyB;
 @property (weak, nonatomic) IBOutlet UILabel *currencyC;
-
+@property (weak, nonatomic) IBOutlet UITableView *lstCurrency;
 
 @end
 
 @implementation CurrencyViewController
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[CRCurrencyResults supportedCurrencies] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    cell.textLabel.text = [[[CRCurrencyResults supportedCurrencies] allObjects] objectAtIndex:indexPath.row];
+    
+    UITextField *txtAmount = [[UITextField alloc] initWithFrame:CGRectMake(100, 13, 200, 30)];
+    txtAmount.placeholder = @"Amount";
+    [txtAmount setClearButtonMode:UITextFieldViewModeWhileEditing];
+    [txtAmount setKeyboardType:UIKeyboardTypeDecimalPad];
+    [txtAmount addTarget:self action:@selector(doConvert) forControlEvents:UIControlEventEditingDidEnd];
+    
+    [cell addSubview:txtAmount];
+    
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"Currencies";
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [[event allTouches] anyObject];
@@ -33,11 +62,18 @@
 
 - (IBAction)buttonTapped:(id)sender {
     self.convertButton.enabled = NO;
-    
     self.req = [[CRCurrencyRequest alloc] init];
+    
     self.req.delegate = self;
     [self.req start];
+}
+
+- (void)doConvert {
+    self.convertButton.enabled = NO;
+    self.req = [[CRCurrencyRequest alloc] init];
     
+    self.req.delegate = self;
+    [self.req start];
 }
 
 - (void)currencyRequest:(CRCurrencyRequest *)req
@@ -52,6 +88,7 @@
     self.currencyA.text = [NSString stringWithFormat:@"%.2f", euroValue];
     self.currencyB.text = [NSString stringWithFormat:@"%.2f", yenValue];
     self.currencyC.text = [NSString stringWithFormat:@"%.2f", gbpValue];
+    
     self.convertButton.enabled = YES;
 }
 
